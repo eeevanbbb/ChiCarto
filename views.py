392536b13b779d9a9d3956cd8b00fc2adbc14264
Evan Bernstein <eeevanbbb@me.com>
2016-05-07 13:31:16 -0500

    user view and template

--- views.py
+++ views.py
@@ -16,7 +16,7 @@ def hello():
 @app.route("/me")
 @login_required
 def me():
-    return 'You are logged in as \"{0}\"'.format(flask_login.current_user.email)
+    return render_template('user.html',user=flask_login.current_user)
 
 @app.route("/search", defaults = {"sid": None})
 @app.route("/search/<sid>")
@@ -33,14 +33,13 @@ def search(sid =  None):
         searches = [dictify_search(search) for search in s]
         json_dict = {'searches': searches }
         return flask.jsonify(**json_dict)
-    
+
 def dictify_search(s):
-    d = {"id": s.id, 
+    d = {"id": s.id,
         "data_sources": [{"id":source.id, "name":source.name}
                         for source in s.data_sources],
         "latitude": s.latitude,
         "longitude": s.longitude,
         "radius": s.radius
     }
-    return d   
-    
+    return d
