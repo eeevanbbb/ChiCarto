8e0d9eb5bd6320c87691a075b2ee81de2d5155a7
Evan Bernstein <eeevanbbb@me.com>
2016-05-08 13:52:58 -0500

    fixed DEBUG script

--- DEBUG_add_data_for_user.py
+++ DEBUG_add_data_for_user.py
@@ -8,10 +8,10 @@ user_email = sys.argv[1]
 user = User.query.filter_by(email=user_email)[0]
 
 uchicago_lat = 41.7886
-uchicago_long = 87.5987
+uchicago_long = -87.5987
 radius = 1000
-url1 = "https://data.cityofchicago.org/resource/energy-usage-2010.json"
-data_source1 = DataSource("Energy Usage 2010",url1,[])
+url1 = "https://data.cityofchicago.org/resource/6zsd-86xi.json"
+data_source1 = DataSource("Crimes 2001 - Present",url1,[],[])
 search1 = Search([data_source1],uchicago_lat,uchicago_long,radius)
 
 user.add_search(search1)
