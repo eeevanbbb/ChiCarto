f1578a899dd8d0189747e58b156de7545b820bd3
Tyler Wojak <tylerwojak@Tylers-MacBook-Pro-3.local>
2016-05-10 03:20:01 -0500

    almost working

--- static/mapping.js
+++ static/mapping.js
@@ -1,19 +1,34 @@
-    function initMap() {
+function initMap() {
     var myLatLng = {lat: 41.8781, lng: -87.6298};
-
-    // Create a map object and specify the DOM element for display.            
+    // Create a map object and specify the DOM element for display.
 
     var map = new google.maps.Map(document.getElementById('map'), {
-            center: myLatLng,
-            scrollwheel: true,
-            zoom: 13
-        });
-
-    // Create a marker and set its position.                                   
-
+        zoom: 4,
+        center: myLatLng
+                                  });
+    
     var marker = new google.maps.Marker({
-            map: map,
-            position: myLatLng,
-            title: 'Hello World!'
-        });
+        position: myLatLng,
+        map: map,
+        title: 'Test Me'
+                                        });
+    
+        var json_file = [{"longitude" = -87.5, "latitude" = 42, "description" =
+                     "test point 1"},
+                     {"longitude" = -87, "latitude" = 41.5, "description" =
+                     "test point 2"}]
+        var marks = new Array();
+        //var json_file = "/search-results/{{ search.id }}" {{ search }};
+        var len = Object.keys(json_file).length;
+        for(i = 0; i < len; i++)
+        {
+            var lat1 = json_file[i].latitude;
+            var lon1 = json_file[i].longitude;
+            var myLatLon = new google.maps.LatLng(lat1,lon1);
+            marks[i] = new google.maps.Marker({
+                position: myLatLon,
+                map: map,
+                title: json_file[i].description
+                                                });
+        }
 }
