86a8ba4703cd8e836b627efc751b371e2d18b630
Mark Sinclair II <125axel125@gmail.com>
2016-05-23 19:37:55 -0500

    Fixed debug script

--- DEBUG_add_data_for_user.py
+++ DEBUG_add_data_for_user.py
@@ -11,16 +11,6 @@ uchicago_lat = 41.7886
 uchicago_long = -87.5987
 radius = 1000
 url1 = "https://data.cityofchicago.org/resource/6zsd-86xi.json"
-# data_source1 = DataSource("Crimes 2001 - Present",url1,[],[])
-# search1 = Search([data_source1],uchicago_lat,uchicago_long,radius)
-
-# user.add_search(search1)
-
-# filter1 = Filter("primary_type","THEFT")
-# data_source2 = DataSource("Crimes 2001 - Present",url1,[filter1],[])
-# search2 = Search([data_source2],uchicago_lat,uchicago_long,radius)
-
-# user.add_search(search2)
 
 data_source1 = DataSource("Crimes 2001 - Present", url1, [])
 
@@ -29,8 +19,8 @@ filter1 = Filter("primary_type", "THEFT")
 data_search1 = DataSearch(data_source1, [])
 data_search2 = DataSearch(data_source1, [filter1])
 
-search1 = Search([data_search1], uchicago_lat, uchicago_long, radius)
-search2 = Search([data_search2], uchicago_lat, uchicago_long, radius)
+search1 = Search([data_search1], uchicago_lat, uchicago_long, radius, 'Sample Search 1')
+search2 = Search([data_search2], uchicago_lat, uchicago_long, radius, 'Sample Search 2')
 
 user.add_search(search1)
 user.add_search(search2)
